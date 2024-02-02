79bdc605414cf4a5879bde1b86794552e02f5459
Daniel <danielrobinson1112@gmail.com>
2024-02-02 14:20:55 +0000

    Improve selection unit tests

--- src/test/java/TestCopySelection.java
+++ src/test/java/TestCopySelection.java
@@ -1,11 +1,13 @@
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.LevelAccessor;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import uk.ac.nottingham.hybridarcade.mechanics.CopySelection;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 public class TestCopySelection {
     CopySelection mCopySelection = null;
@@ -17,119 +19,126 @@ public class TestCopySelection {
         mLevel = mock(LevelAccessor.class);
     }
 
-    /**** Basics ****/
+    @Nested
+    class TestBasics {
+        final static BlockPos[] EXAMPLE_VERTICES = {new BlockPos(1, 2, 3),
+                new BlockPos(10, 11, 12), new BlockPos(21, 22, 23), new BlockPos(31, 32, 33),
+                new BlockPos(41, 42, 43), new BlockPos(51, 52, 53), new BlockPos(61, 62, 63)};
 
-    final static BlockPos[] EXAMPLE_VERTICES = {new BlockPos(1,2,3),
-            new BlockPos(10,11,12), new BlockPos(21,22,23), new BlockPos(31,32,33),
-            new BlockPos(41,42,43), new BlockPos(51,52,53), new BlockPos(61,62,63)};
-
-    @Test
-    public void testAddVertex() {
-        for(int i = 0; i < 4; i++){
-            mCopySelection.addMarker(EXAMPLE_VERTICES[i]);
+        @Test
+        public void testAddVertex() {
+            for (int i = 0; i < 4; i++) {
+                mCopySelection.addMarker(EXAMPLE_VERTICES[i]);
+            }
+            for (int i = 0; i < 4; i++) {
+                assertEquals(EXAMPLE_VERTICES[i], mCopySelection.getMarker(i));
+            }
         }
-        for(int i = 0; i < 4; i++){
-            assertEquals(EXAMPLE_VERTICES[i], mCopySelection.getMarker(i));
+
+        @Test
+        public void testVerticesClearWhenFull() {
+            for (int i = 0; i < 4; i++) {
+                mCopySelection.addMarker(EXAMPLE_VERTICES[0]);
+            }
+            mCopySelection.addMarker(EXAMPLE_VERTICES[4]);
+
+            assertEquals(EXAMPLE_VERTICES[4], mCopySelection.getMarker(0));
+            assertNull(mCopySelection.getMarker(1));
+            assertNull(mCopySelection.getMarker(2));
+            assertNull(mCopySelection.getMarker(3));
+        }
+
+        @Test
+        public void testGetVertexAsString() {
+            mCopySelection.addMarker(new BlockPos(100, 200, 300));
+            assertEquals("[x100,y200,z300]", mCopySelection.getMarkerAsString(0));
+        }
+
+        @Test
+        public void testGetVertexOutOfIndex() {
+            assertNull(mCopySelection.getMarker(999));
         }
     }
 
-    @Test
-    public void testVerticesClearWhenFull() {
-        for(int i = 0; i < 4; i ++){
-            mCopySelection.addMarker(EXAMPLE_VERTICES[0]);
+    @Nested
+    class TestMaths {
+        @Test
+        public void testInvalidSelectionDiagonal() {
+            mCopySelection.addMarker(new BlockPos(0, 0, 0));
+            mCopySelection.addMarker(new BlockPos(1, 0, 0));
+            mCopySelection.addMarker(new BlockPos(0, 1, 1));
+            mCopySelection.addMarker(new BlockPos(0, 1, 0));
+            int numStored = mCopySelection.copyAndPrintBlocks(mLevel);
+            verify(mLevel, times(0)).getBlockState(any());
+            assertEquals(0, numStored);
         }
-        mCopySelection.addMarker(EXAMPLE_VERTICES[4]);
 
-        assertEquals(EXAMPLE_VERTICES[4], mCopySelection.getMarker(0));
-        assertNull(mCopySelection.getMarker(1));
-        assertNull(mCopySelection.getMarker(2));
-        assertNull(mCopySelection.getMarker(3));
-    }
+        @Test
+        public void testInvalidSelectionSquare() {
+            mCopySelection.addMarker(new BlockPos(0, 0, 0));
+            mCopySelection.addMarker(new BlockPos(1, 0, 0));
+            mCopySelection.addMarker(new BlockPos(0, 1, 0));
+            mCopySelection.addMarker(new BlockPos(1, 1, 0));
+            int numStored = mCopySelection.copyAndPrintBlocks(mLevel);
+            verify(mLevel, times(0)).getBlockState(any());
+            assertEquals(0, numStored);
+        }
 
-    @Test
-    public void testGetVertexAsString() {
-        mCopySelection.addMarker(new BlockPos(100,200,300));
-        assertEquals("[x100,y200,z300]", mCopySelection.getMarkerAsString(0));
-    }
+        @Test
+        public void testInvalidSelectionStraightLine() {
+            mCopySelection.addMarker(new BlockPos(0, 0, 1));
+            mCopySelection.addMarker(new BlockPos(0, 0, 2));
+            mCopySelection.addMarker(new BlockPos(0, 0, 3));
+            mCopySelection.addMarker(new BlockPos(0, 0, 4));
+            int numStored = mCopySelection.copyAndPrintBlocks(mLevel);
+            verify(mLevel, times(0)).getBlockState(any());
+            assertEquals(0, numStored);
+        }
 
-    @Test
-    public void testGetVertexOutOfIndex() {
-        assertNull(mCopySelection.getMarker(999));
-    }
+        @Test
+        public void testInvalidSelectionNullValues() {
+            mCopySelection.addMarker(new BlockPos(0, 0, 0));
+            mCopySelection.addMarker(null);
+            mCopySelection.addMarker(null);
+            mCopySelection.addMarker(null);
+            int numStored = mCopySelection.copyAndPrintBlocks(mLevel);
+            verify(mLevel, times(0)).getBlockState(any());
+            assertEquals(0, numStored);
+        }
 
-    /**** Mathsy Stuff ****/
+        @Test
+        public void testStoresCorrectNumberOfBlocksUnitCube() {
+            mCopySelection.addMarker(new BlockPos(0, 0, 0));
+            mCopySelection.addMarker(new BlockPos(1, 0, 0));
+            mCopySelection.addMarker(new BlockPos(0, 0, 1));
+            mCopySelection.addMarker(new BlockPos(0, 1, 0));
+            int numStored = mCopySelection.copyAndPrintBlocks(mLevel);
+            verify(mLevel, times(8)).getBlockState(any());
+            assertEquals(8, numStored);
+        }
 
-    @Test
-    public void testValidSelection() {
-        // Standard
-        mCopySelection.addMarker(new BlockPos(0,0,0));
-        mCopySelection.addMarker(new BlockPos(1,0,0));
-        mCopySelection.addMarker(new BlockPos(0,0,1));
-        mCopySelection.addMarker(new BlockPos(0,1,0));
-        assertNotEquals(0, mCopySelection.copyAndPrintBlocks(mLevel));
+        @Test
+        public void testStoresCorrectNumberOfBlocksNegativeUnitCube() {
+            mCopySelection.addMarker(new BlockPos(0, 0, 0));
+            mCopySelection.addMarker(new BlockPos(-1, 0, 0));
+            mCopySelection.addMarker(new BlockPos(0, 0, 1));
+            mCopySelection.addMarker(new BlockPos(0, -1, 0));
+            int numStored = mCopySelection.copyAndPrintBlocks(mLevel);
+            verify(mLevel, times(8)).getBlockState(any());
+            assertEquals(8, numStored);
+        }
 
-        // From a different direction
-        mCopySelection.addMarker(new BlockPos(50,50,50));
-        mCopySelection.addMarker(new BlockPos(50,30,50));
-        mCopySelection.addMarker(new BlockPos(80,50,50));
-        mCopySelection.addMarker(new BlockPos(50,50,10));
-        assertNotEquals(0, mCopySelection.copyAndPrintBlocks(mLevel));
-    }
-
-    @Test
-    public void testInvalidSelection() {
-        // Diagonal
-        mCopySelection.addMarker(new BlockPos(0,0,0));
-        mCopySelection.addMarker(new BlockPos(1,0,0));
-        mCopySelection.addMarker(new BlockPos(0,1,1));
-        mCopySelection.addMarker(new BlockPos(0,1,0));
-        assertEquals(0, mCopySelection.copyAndPrintBlocks(mLevel));
-
-        // Square
-        mCopySelection.addMarker(new BlockPos(0,0,0));
-        mCopySelection.addMarker(new BlockPos(1,0,0));
-        mCopySelection.addMarker(new BlockPos(0,1,0));
-        mCopySelection.addMarker(new BlockPos(1,1,0));
-        assertEquals(0, mCopySelection.copyAndPrintBlocks(mLevel));
-
-        // Straight Line
-        mCopySelection.addMarker(new BlockPos(0,0,1));
-        mCopySelection.addMarker(new BlockPos(0,0,2));
-        mCopySelection.addMarker(new BlockPos(0,0,3));
-        mCopySelection.addMarker(new BlockPos(0,0,4));
-        assertEquals(0, mCopySelection.copyAndPrintBlocks(mLevel));
-
-        // Null values
-        mCopySelection.addMarker(new BlockPos(0,0,0));
-        mCopySelection.addMarker(null);
-        mCopySelection.addMarker(null);
-        mCopySelection.addMarker(null);
-        assertEquals(0, mCopySelection.copyAndPrintBlocks(mLevel));
-    }
-
-    @Test
-    public void testStoresCorrectNumberOfBlocks() {
-        // Unit Cube
-        mCopySelection.addMarker(new BlockPos(0,0,0));
-        mCopySelection.addMarker(new BlockPos(1,0,0));
-        mCopySelection.addMarker(new BlockPos(0,0,1));
-        mCopySelection.addMarker(new BlockPos(0,1,0));
-        assertEquals(8, mCopySelection.copyAndPrintBlocks(mLevel));
-
-        // Negative Unit Cube
-        mCopySelection.addMarker(new BlockPos(0,0,0));
-        mCopySelection.addMarker(new BlockPos(-1,0,0));
-        mCopySelection.addMarker(new BlockPos(0,0,1));
-        mCopySelection.addMarker(new BlockPos(0,-1,0));
-        assertEquals(8, mCopySelection.copyAndPrintBlocks(mLevel));
-
-        // Cuboid
-        // Remember +1 is added to the max of each so the marker itself is included
-        mCopySelection.addMarker(new BlockPos(0,0,0));
-        mCopySelection.addMarker(new BlockPos(-9,0,0));
-        mCopySelection.addMarker(new BlockPos(0,0,29));
-        mCopySelection.addMarker(new BlockPos(0,19,0));
-        assertEquals(6000, mCopySelection.copyAndPrintBlocks(mLevel));
+        @Test
+        public void testStoresCorrectNumberOfBlocksCuboid() {
+            // Remember +1 is added to the max of each so the marker itself is included
+            mCopySelection.addMarker(new BlockPos(0, 0, 0));
+            mCopySelection.addMarker(new BlockPos(-9, 0, 0));
+            mCopySelection.addMarker(new BlockPos(0, 0, 29));
+            mCopySelection.addMarker(new BlockPos(0, 19, 0));
+            int numStored = mCopySelection.copyAndPrintBlocks(mLevel);
+            verify(mLevel, times(6000)).getBlockState(any());
+            assertEquals(6000, numStored);
+        }
     }
 
 }
--- src/test/java/TestPasteSelection.java
+++ src/test/java/TestPasteSelection.java
@@ -1,4 +1,60 @@
-public class TestPasteSelection {
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.state.BlockState;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import uk.ac.nottingham.hybridarcade.Utility;
+import uk.ac.nottingham.hybridarcade.mechanics.PasteSelection;
 
-    //TODO: this file
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class TestPasteSelection {
+    private final BlockPos CENTER_POSITION = new BlockPos(0,0,0);
+    PasteSelection mPasteSelection = null;
+    LevelAccessor mLevel = null;
+
+    @BeforeEach
+    public void setup() {
+        mPasteSelection = new PasteSelection();
+        mLevel = mock(LevelAccessor.class);
+    }
+
+    @Test
+    public void testInvalidSelectionNull() {
+        BlockState blocks[][][] = null;
+        Utility.debugBlocks = blocks; // TODO: replace with mock
+        mPasteSelection.scanAndStoreBlocks();
+        int numStored = mPasteSelection.pasteBlocks(mLevel, CENTER_POSITION);
+        verify(mLevel, times(0)).setBlock(any(), any(), anyInt());
+        assertEquals(0, numStored);
+    }
+
+    @Test
+    public void testPastesSelectionBasic() {
+        BlockState[][][] blocks = new BlockState[2][2][2];
+        Utility.debugBlocks = blocks; // TODO: replace with mock
+        mPasteSelection.scanAndStoreBlocks();
+        int numStored = mPasteSelection.pasteBlocks(mLevel, CENTER_POSITION);
+        verify(mLevel, times(8)).setBlock(any(), any(), anyInt());
+        assertEquals(8, numStored);
+    }
+
+    @Test
+    public void testPastesSelectionInCorrectPlace() {
+        BlockState[][][] blocks = new BlockState[2][2][2];
+        Utility.debugBlocks = blocks; // TODO: replace with mock
+        mPasteSelection.scanAndStoreBlocks();
+        int numStored = mPasteSelection.pasteBlocks(mLevel, new BlockPos(10,10,10));
+        verify(mLevel).setBlock(eq(new BlockPos(10,10,10)), any(), anyInt());
+        verify(mLevel).setBlock(eq(new BlockPos(10,10,11)), any(), anyInt());
+        verify(mLevel).setBlock(eq(new BlockPos(10,11,10)), any(), anyInt());
+        verify(mLevel).setBlock(eq(new BlockPos(10,11,11)), any(), anyInt());
+        verify(mLevel).setBlock(eq(new BlockPos(11,10,10)), any(), anyInt());
+        verify(mLevel).setBlock(eq(new BlockPos(11,10,11)), any(), anyInt());
+        verify(mLevel).setBlock(eq(new BlockPos(11,11,10)), any(), anyInt());
+        verify(mLevel).setBlock(eq(new BlockPos(11,11,11)), any(), anyInt());
+        assertEquals(8, numStored);
+    }
 }
